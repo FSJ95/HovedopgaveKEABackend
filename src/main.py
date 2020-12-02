d6cad0914b4bbaa12eaf638d1208299537193087
oliverlowbob <oliverlowbob@live.dk>
2020-12-02 23:43:11 +0100

    fetching feeds

--- src/main.py
+++ src/main.py
@@ -3,27 +3,49 @@ from models.link import Link
 from fastapi import FastAPI
 import re
 from urllib.request import urlopen
+import requests
 
 app = FastAPI()
+
+allowedFeedTypes = ["xml"]
+allowedLinkTypes = ["csv", "json", "xml"]
 
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
+
+@app.get("/upload/feed")
+def fetch_feed(link: Link):
+
+    url = link.url
+
+    r = requests.get(url, allow_redirects=True)
+
+    ext = r.headers['content-type'].split('/')[-1].split(";")[0] # get extension of file and character and splits again to only get extension
+
+    if ext not in allowedFeedTypes:
+        return {"Status": "Error: Feed is not a supported filetype"}
+
+    with urlopen(url) as x:
+        data = x.read().decode('utf-8')
+    
+    with open('/Users/olive/Desktop/'+"feed."+ext, 'w') as f:
+        f.write(data)   
+
+    return {"Status": "Sucess"}
 
 @app.get("/upload/link/")
 def fetch_file(link: Link):
 
     url = link.url
 
-    allowedTypes = ["csv", "json", "xml"]
-
     if url.find('/'):
         nameAndType = url.rsplit('/', 1)[1]
         print(nameAndType)
         onlyType = nameAndType.rsplit('.', 1)[1]
         print(onlyType)
-        if onlyType not in allowedTypes:
-            return {"Error": "Filetype not allowed"}          
+        if onlyType not in allowedLinkTypes:
+            return {"Status": "Error: "+onlyType+" is not supported"}          
 
     with urlopen(url) as x:
         data = x.read().decode('utf-8')
@@ -35,4 +57,4 @@ def fetch_file(link: Link):
     # with open('/Users/olive/Desktop/'+nameAndType, 'w') as f:
     #     f.write(data)
 
-    return {"Download": "Suceeded"}
+    return {"Status": "Sucess"}
