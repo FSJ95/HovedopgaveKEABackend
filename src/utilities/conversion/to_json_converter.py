68fea08f0fb2daaf024bef542e5c24507127990e
Tobias <tobias@wecode.dk>
2020-12-04 14:09:23 +0100

    Staged for merge

--- src/utilities/conversion/to_json_converter.py
+++ src/utilities/conversion/to_json_converter.py
@@ -10,25 +10,18 @@ def xml_stream_to_json(xml_stream):
         Takes an xml string and converts to json format in utf-8
     """
 
-    xml_json = xmltodict.parse(xml_stream)
- 
-    jsonDump = json.dumps(xml_json, ensure_ascii=False)
-
-    with open('./'+"test.json", 'w', encoding='utf8') as f:
-        f.write(jsonDump)   
-    
-    return {"Status": "Sucess"}
+    try:
+        xml_json = xmltodict.parse(xml_stream)
+        return json.dumps(xml_json, ensure_ascii=False)
+    except Exception as identifier:
+        return False
 
 def csv_stream_to_json(csv_stream):
     """
         Takes a csv file path, reads file and converts to json format
     """
-
-    csv_json = csv.DictReader(io.StringIO(csv_stream))
- 
-    jsonDump = json.dumps(list(csv_json), ensure_ascii=False)
-
-    with open('./'+"test.json", 'w', encoding='utf8') as f:
-        f.write(jsonDump)
-    
-    return {"Status": "Sucess"}
+    try:
+        csv_json = csv.DictReader(io.StringIO(csv_stream))
+        return json.dumps(list(csv_json), ensure_ascii=False)
+    except Exception as identifier:
+        return False
