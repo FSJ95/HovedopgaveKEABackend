3ff389a9f92916d7f859c60609ed822c73fb81da
Tobias <tobias@wecode.dk>
2020-12-07 13:45:21 +0100

    Refactor default filename

--- src/controllers/amazoncontroller.py
+++ src/controllers/amazoncontroller.py
@@ -15,22 +15,22 @@ def getClient():
                         aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
                         region_name=REGION_NAME)
 
-def upload_file(content, object_name=None):
+def upload_file(content, file_name=None):
     """Upload a file to an S3 bucket
 
-    :param file_name: File to upload
+    :param content: content to upload
     :param bucket: Bucket to upload to
-    :param object_name: S3 object name. If not specified then file_name is used
+    :param file_name: S3 object name. If not specified then content is used
     :return: True if file was uploaded, else False
     """ 
 
     # If S3 object_name was not specified, use file_name
-    if object_name is None:
-        object_name = content
+    if file_name is None:
+        file_name = content[0:10] + '.json'
 
     # Upload the file
     try:
-        getClient().put_object(Body=content, Bucket=BUCKET, Key=object_name, ACL='public-read')
+        getClient().put_object(Body=content, Bucket=BUCKET, Key=file_name, ACL='public-read')
     except ClientError as e:
          logging.error(e)
          return False
