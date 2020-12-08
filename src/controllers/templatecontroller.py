d5db7f9da30d9e7bc2398f12d9f93326ee78ffeb
oliverlowbob <oliverlowbob@live.dk>
2020-12-08 10:09:08 +0100

    Add more database handling

--- src/controllers/templatecontroller.py
+++ src/controllers/templatecontroller.py
@@ -7,7 +7,19 @@ import psycopg2
 
 def get_template(template_id: int):
 
-    return "Should return template with id"+template_id
+    # Connect to your postgres DB
+    conn = psycopg2.connect("dbname=database-2 user=postgres password=jojaerklam host=database-2.ca7pqa3dikbd.us-east-1.rds.amazonaws.com port=5432")
+
+    # Open a cursor to perform database operations
+    cur = conn.cursor()
+
+    # Execute a query
+    cur.execute(f"SELECT * FROM templates WHERE id={template_id}")
+
+    # Retrieve query results
+    records = cur.fetchall()
+    
+    return records
 
 def get_all_templates():
     
@@ -32,4 +44,20 @@ def update_template(template_id: int, templateArgs: TemplateArgs):
 
 def delete_template(template_id: int):
 
-    return {"template to be deleted: template id": template_id}
+    # Connect to your postgres DB
+    conn = psycopg2.connect("dbname=database-2 user=postgres password=jojaerklam host=database-2.ca7pqa3dikbd.us-east-1.rds.amazonaws.com port=5432")
+
+    # Open a cursor to perform database operations
+    cur = conn.cursor()
+
+    # Execute a query
+    cur.execute(f"DELETE FROM templates WHERE id = {template_id}")
+
+    #Save the change
+    conn.commit()
+
+    #Close cursor and connection
+    cur.close()
+    conn.close()
+
+    return {"Status" : "Success"}
